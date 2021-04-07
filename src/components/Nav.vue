31b78e1f54367f3188b69308a9985c3876be3564
maotterson <otterson.2@gmail.com>
2021-04-06 20:26:49 -0400

    decouple routes and nav components

---
+++ src/components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-navigation-drawer permanent>
+        <v-list
+            dense
+            nav
+        >
+            <router-link 
+                v-for="route in routes" 
+                :key="route.name"
+                tag="v-list-item" 
+                :to="route.path"
+            >
+                {{route.name}}
+            </router-link>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    computed: {
+        routes : function() {
+            return this.$router.options.routes;
+        }
+    }
+    
+}
+</script>
+<style scoped>
+
+</style>
